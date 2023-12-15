3612838727f6cdba39f9ea7653892622dfd5313e
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-12-15 21:11:58 +0000

    Add token in.

--- .github/actions/junit-report/index.js
+++ .github/actions/junit-report/index.js
@@ -4,12 +4,16 @@ const github = require('@actions/github');
 try
 {
     const context = github.context;
-    github.getOctokit().rest.issues.createComment(
+    const token = core.getInput('repo-token');
+    const octokit = github.getOctokit(token);
+    core.info("Hello from junit report");
+    octokit.rest.issues.createComment(
         {
-            issue_number: context.issue.number,
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            body: 'Hello from action'
+            issue_number: context.issue().number,
+            owner: context.repo().owner,
+            repo: context.repo().repo,
+            body: 'Hello from action',
+
         }
     )
 }
