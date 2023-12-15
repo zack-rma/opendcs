e742f0238c0fe118ed1d78d11091c28eccea455f
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-12-15 21:39:09 +0000

    Convert to typescript.

---
+++ .github/actions/junit-report/src/index.ts
@@ -0,0 +1,27 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {Context} from '@actions/github/lib/context'
+
+try
+{
+    const context: Context = github.context;
+    const token: string = core.getInput('repo-token');
+    const octokit: any = github.getOctokit(token);
+    core.info("Hello from junit report");
+    octokit.rest.issues.createComment(
+        {
+            issue_number: context.issue.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body: 'Hello from action',
+
+        }
+    )
+}
+catch (error)
+{
+    if (error instanceof Error)
+    {
+        core.setFailed(error.message);
+    }
+}
